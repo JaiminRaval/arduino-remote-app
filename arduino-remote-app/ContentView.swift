0c219307bc05301c9fe86b29c4c1d511ab84d639
JaiminRaval <siliconvalley.jaimin@gmail.com>
2024-07-22 19:16:41 +0530

    Initial Commit

---
+++ arduino-remote-app/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  arduino-remote-app
+//
+//  Created by Jaimin Raval on 22/07/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ arduino-remote-app/arduino_remote_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  arduino_remote_appApp.swift
+//  arduino-remote-app
+//
+//  Created by Jaimin Raval on 22/07/24.
+//
+
+import SwiftUI
+
+@main
+struct arduino_remote_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
